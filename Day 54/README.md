210931213c51fadfa9f7edde17402280bc715448
Abhishek Sabnivis <abhisabnives@gmail.com>
2021-06-24 21:13:19 +0530

    Completed Day 54

---
+++ Day 54/README.md
@@ -0,0 +1,5 @@
+## Applications:
+- Create a basic python decorator
+
+### Concepts:
+- Intro to Backend Web Development using Flask
---
+++ Day 54/main.py
@@ -0,0 +1,22 @@
+import time
+
+def speed_calc_decorator(function):
+    def wrapper_function():
+        start = time.time()
+        function()
+        end = time.time()
+        print(f"Time taken to run {function.__name__} is {end-start} seconds")
+    return wrapper_function
+
+@speed_calc_decorator
+def fast_function():
+    for i in range(10000000):
+        i * i
+        
+@speed_calc_decorator
+def slow_function():
+    for i in range(100000000):
+        i * i
+
+fast_function()
+slow_function()
