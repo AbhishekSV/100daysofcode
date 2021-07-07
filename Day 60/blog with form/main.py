9ee1ae2735d1d4ae5f84b9dc7e97455aa84c304e
Abhishek Sabnivis <abhisabnives@gmail.com>
2021-07-08 00:46:10 +0530

    Completed Day 60 code

--- Day 60/blog with form/main.py
+++ Day 60/blog with form/main.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
+import os
 
 posts = requests.get("https://api.npoint.io/26b9e8c41536f5f06765").json()
+EMAIL_ID = os.environ.get('EMAIL_ID')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 app = Flask(__name__)
 
@@ -11,9 +15,26 @@ def get_all_posts():
     return render_template("index.html", all_posts=posts)
 
 
-@app.route('/<filename>')
-def redirect(filename):
-    return render_template(filename)
+@app.route('/about')
+def redirect():
+    return render_template("about.html")
+
+
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New Message\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage:{message}"
+    with smtplib.SMTP("smtp.gmail.com:587") as connection:
+        connection.starttls()
+        connection.login(user=EMAIL_ID, password=EMAIL_PASSWORD)
+        connection.sendmail(from_addr=EMAIL_ID, to_addrs=EMAIL_ID, msg=email_message)
 
 
 @app.route("/post/<int:index>")
